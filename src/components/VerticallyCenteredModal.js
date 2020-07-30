b6e48658f96b67f57b4ddca33ead99014e74efaa
James Cowart <james@Jamess-Macbook.local>
2020-07-29 23:36:24 -0400

    deployedgit add .! and removed 2 registry buttons

--- src/components/VerticallyCenteredModal.js
+++ src/components/VerticallyCenteredModal.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Modal, Button, Card, Row, Container, Col } from 'react-bootstrap';
-import amazon from './../images/amazon.png'
+// import amazon from './../images/amazon.png'
 import bed_bath_beyond from './../images/bed_bath_beyond.png';
 import dillards from './../images/dillards.png';
-import lowes from './../images/lowes.png';
+// import lowes from './../images/lowes.png';
 
 function VerticallyCenteredModal(props) {
     return (
@@ -23,34 +23,34 @@ function VerticallyCenteredModal(props) {
         <div className='container-fluid d-flex py-4 justify-content-around align-items-center'>
           <Container>
             <Row className="justify-content-md-center">
-              <Col xs={6} md={3}>
+              {/* <Col xs={6} md={3}>
                 <a target='_blank' rel='noopener noreferrer' href='www.jamesandbecks.us/#'>
                   <Card>
                     <Card.Img variant="top" src={amazon} />
                   </Card>
                 </a>
-              </Col>
-              <Col xs={6} md={3}>
+              </Col> */}
+              <Col xs={12} md={6}>
                 <a target='_blank' rel='noopener noreferrer' href='https://www.bedbathandbeyond.com/store/giftregistry/viewregistryguest/549077696?eventType=Wedding'>
                   <Card>
                     <Card.Img variant="top" src={bed_bath_beyond} />
                   </Card>
                 </a>
               </Col>
-              <Col xs={6} md={3}>
+              <Col xs={12} md={6}>
                 <a target='_blank' rel='noopener noreferrer' href='https://www.dillards.com/webapp/wcs/stores/servlet/RegistryManageProducts?catalogId=301&langId=-1&storeId=301&actionCode=M&registryNumber=132821638'>
                   <Card>
                     <Card.Img variant="top" src={dillards} />
                   </Card>
                 </a>
               </Col>
-              <Col xs={6} md={3}>
+              {/* <Col xs={6} md={3}>
                 <a target='_blank' rel='noopener noreferrer' href='www.jamesandbecks.us/#'>
                   <Card>
                     <Card.Img variant="top" src={lowes} />
                   </Card>
                 </a>
-              </Col>
+              </Col> */}
             </Row>
           </Container>
         </div>
